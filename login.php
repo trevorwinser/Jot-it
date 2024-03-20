90e9de6597b6b3d76f3abdc33242309bfbb53d7d
trevorwinser <64448847+trevorwinser@users.noreply.github.com>
2024-03-20 10:42:34 -0700

    Login Verification

--- login.php
+++ login.php
@@ -7,17 +7,30 @@ $dbname = "jot-it";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 
-if(isset($_POST["username"]) && isset($_POST["password"])) {
+if(isset($_POST["username"]) && isset($_POST["password"]) && !empty($_POST["username"]) && !empty($_POST["password"])) {
     $sql = "SELECT * FROM User WHERE username = ? AND password = ?";
     $stmt = $conn->prepare($sql);
 
-    $stmt->bind_param("sss", $username, $password);
+    $stmt->bind_param('ss', $username, $password);
 
     $username = $_POST["username"];
     $password = $_POST["password"];
     
     if ($stmt->execute()) {
-        echo "New record created successfully";
+        $result = $stmt->get_result();
+        $result = $stmt->get_result();
+        while ($row = $result->fetch_assoc()) {
+            if (strcmp($row['username'], $username) == 0 && strcmp($row['password'], password_hash($password, PASSWORD_DEFAULT)) == 0) {
+                session_start();
+                $_SESSION['username'] = $username;      // Can access across pages.
+            } else {
+                if (strcmp($row['username'], $username) == 0) {
+                    echo "<p>Username could not be found</p>";
+                } else {
+                    echo "<p>Password is incorrect"
+                }
+            }
+        }
     } else {
         echo "Error: " . $sql . "<br>" . $stmt->error;
     }
@@ -25,7 +38,7 @@ if(isset($_POST["username"]) && isset($_POST["password"])) {
     $stmt->close();
 } else {
     echo "<p>All fields are required</p>";
-    if (isset($_POST["username"]))
+    if (isset($_POST["username"]) || !empty($_POST["username"]))
         echo "<p>Password is missing</p>";
     else
         echo "<p>Username is missing</p>";
