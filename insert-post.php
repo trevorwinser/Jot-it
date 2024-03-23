cb2118b119279133559ea8b7470c04353133bc27
Komal <ksingh0925@gmail.com>
2024-03-23 13:29:06 -0700

    validation checks based on our db

--- insert-post.php
+++ insert-post.php
@@ -12,12 +12,22 @@ if ($conn->connect_error) {
 }
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    $title = $_POST['title'];
-    $post = $_POST['post'];
+    $title = trim($_POST['title']);
+    $post = trim($_POST['post']);
     $datetime = date('Y-m-d H:i:s');
+
+    if (strlen($title) > 100 || strlen($post) > 1000) {
+        die("Title or post body exceeds allowed length.");
+    }
+
+    $image = NULL;
 
     // Check if a file was uploaded
     if (isset($_FILES['image']) && $_FILES['image']['error'] == UPLOAD_ERR_OK) {
+        $imageInfo = getimagesize($_FILES['image']['tmp_name']);
+        if ($imageInfo === FALSE) {
+            die("Uploaded file is not a valid image.");
+        }
         // Open the file and read its contents
         $tmpName = $_FILES['image']['tmp_name'];
         $fp = fopen($tmpName, 'rb'); // Read binary data
--- register.php
+++ register.php
@@ -8,33 +8,26 @@
     <link rel="stylesheet" href="css/nav.css">
 </head>
 <body>
-    <?php
-        include 'navbar.php';
-    ?>
+    <?php include 'navbar.php'; ?>
+
     <table>
-        <tr>
-            <th>Register</th>
-        </tr>
+        <tr><th>Register</th></tr>
         <tr>
             <td>
-                <?php
-                if(isset($_GET['message'])) {
-                    echo '<p id="error">' . $_GET['message'] . '</p>';
-                }
-                ?>
+                <?php if (isset($_GET['message'])) { echo '<p id="error">' . htmlspecialchars($_GET['message']) . '</p>'; } ?>
             </td>
         </tr>
         <form action="checkregister.php" method="POST">
         <tr>
             <td>
-                <label for="fname">Enter a Username:&nbsp&nbsp</label>
-                <input type="text" id="username" name="username">
+                <label for="username">Enter a Username:&nbsp;&nbsp;</label>
+                <input type="text" id="username" name="username" pattern="[a-zA-Z0-9]{5,40}" title="Username should be 5-40 characters long and include only letters and numbers." required>
             </td>
         </tr>
         <tr>
             <td>
-                <label for="lname">Enter a Password:&nbsp&nbsp&nbsp</label>
-                <input type="password" id="password" name="password"><br>
+                <label for="password">Enter a Password:&nbsp;&nbsp;&nbsp;</label>
+                <input type="password" id="password" name="password" minlength="8" required><br>
             </td>
         </tr>
         <tr>
