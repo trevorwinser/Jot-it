1c75ead0fed288fee2266c11a23baa3fa6f6740e
Komal <>
2024-04-11 22:45:46 -0700

    Update conn.php

--- conn.php
+++ conn.php
@@ -5,7 +5,7 @@ $username = "61837175";
 $password = "61837175";
 $dbname = "db_61837175";
 
-    $conn = new mysqli($servername, $db_username, $db_password, $dbname);
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
